a042ec70cd455722c08cc579406dc494702644d1
Harold Karibiye <tkaribiye@microsoft.com>
2025-03-25 17:59:37 +0000

    fix spacing

--- Models/TsgModel.cs
+++ Models/TsgModel.cs
@@ -5,8 +5,6 @@ public class TsgModel
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-
     public List<string> Steps { get; set; } = new();
-
     public List<string> Tags { get; set; } = new();
 }
