81fdf37bd5e94077ae64a5760f8826b3f442da1d
Harold Karibiye <tkaribiye@microsoft.com>
2025-03-26 14:13:29 +0000

    add incidents hub

--- Services/Worker.cs
+++ Services/Worker.cs
@@ -48,7 +48,7 @@ public class Worker
             }
             else
             {
-                await Task.Delay(500, token); // Wait before checking the queue again
+                await Task.Delay(100, token); // Wait before checking the queue again
             }
         }
     }
